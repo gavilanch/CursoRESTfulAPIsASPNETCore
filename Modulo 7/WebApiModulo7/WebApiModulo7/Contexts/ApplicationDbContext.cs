6449e9e8ac9d175f0dd960dcd34078e3639c9d45
Felipe Gavilan <elmejor_887@hotmail.com>
2019-02-08 22:36:00 -0400

    Module 7 added

---
+++ Modulo 7/WebApiModulo7/WebApiModulo7/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiModulo7.Models;
+
+namespace WebApiModulo7.Contexts
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ Modulo 7/WebApiModulo7/WebApiModulo7/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiModulo7.Models
+{
+    public class UserInfo
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
