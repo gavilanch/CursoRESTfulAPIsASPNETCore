0703c078f454e328c03b097650c43e8397ebb9dd
Felipe Gavilan <elmejor_887@hotmail.com>
2019-02-28 21:55:50 -0400

    Agregado modulo 10

---
+++ Modulo 10/WebApiModulo10/WebApiModulo10/Controllers/AutoresController.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiModulo10.Contexts;
+using WebApiModulo10.Entities;
+using WebApiModulo10.Models;
+
+namespace WebApiModulo10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public AutoresController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        // GET api/autores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AutorDTO>>> Get()
+        {
+            var autores = await context.Autores.ToListAsync();
+            var autoresDTO = mapper.Map<List<AutorDTO>>(autores);
+            return autoresDTO;
+        }
+
+        // GET api/autores/5 
+        [HttpGet("{id}", Name = "ObtenerAutor")]
+        public async Task<ActionResult<AutorDTO>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            var autorDTO = mapper.Map<AutorDTO>(autor);
+
+            return autorDTO;
+        }
+
+        // POST api/autores
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] AutorCreacionDTO autorCreacion)
+        {
+            var autor = mapper.Map<Autor>(autorCreacion);
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            var autorDTO = mapper.Map<AutorDTO>(autor);
+            return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id }, autorDTO);
+        }
+
+        // PUT api/autores/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] AutorCreacionDTO autorActualizacion)
+        {
+            var autor = mapper.Map<Autor>(autorActualizacion);
+            autor.Id = id;
+            context.Entry(autor).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> Patch(int id, [FromBody] JsonPatchDocument<AutorCreacionDTO> patchDocument)
+        {
+            if (patchDocument == null)
+            {
+                return BadRequest();
+            }
+
+            var autorDeLaDB = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autorDeLaDB == null)
+            {
+                return NotFound();
+            }
+
+            var autorDTO = mapper.Map<AutorCreacionDTO>(autorDeLaDB);
+
+            patchDocument.ApplyTo(autorDTO, ModelState);
+
+            var isValid = TryValidateModel(autorDeLaDB);
+
+            if (!isValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            mapper.Map(autorDTO, autorDeLaDB);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+        // DELETE api/autores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var autorId = await context.Autores.Select(x => x.Id).FirstOrDefaultAsync(x => x == id);
+
+            if (autorId == default(int))
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor { Id = autorId });
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
---
+++ Modulo 10/WebApiModulo10/WebApiModulo10/Entities/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiModulo10.Entities
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Identificacion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public List<Libro> Books { get; set; }
+    }
+}
