d29e4c32c1dfa0e4543794be328bf0afe1e9b5f2
Felipe Gavilan <elmejor_887@hotmail.com>
2019-11-13 17:20:27 -0400

    Modulo 5 - ASP.NET Core 3.0

---
+++ ASP.NET Core 3.0/Modulo 5/WebApiModulo5/WebApiModulo5/Migrations/20190121123418_Datos-Libros.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiModulo5.Migrations
+{
+    public partial class DatosLibros : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Libros",
+                columns: new[] { "Id", "AutorId", "Titulo" },
+                values: new object[] { 1, 1, "Entity Framework Core 2.1 - De verdad" });
+
+            migrationBuilder.InsertData(
+                table: "Libros",
+                columns: new[] { "Id", "AutorId", "Titulo" },
+                values: new object[] { 2, 2, "Construyendo Web APIs con ASP.NET Core 2.2" });
+
+            migrationBuilder.InsertData(
+                table: "Libros",
+                columns: new[] { "Id", "AutorId", "Titulo" },
+                values: new object[] { 3, 2, "Libro de prueba" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Libros",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Libros",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Libros",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
