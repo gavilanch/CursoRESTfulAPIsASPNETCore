239eba33ea6b4fdc5938a3e87b3a4093b93e099c
Felipe Gavilan <elmejor_887@hotmail.com>
2021-05-20 05:43:36 -0400

    Agregado modulo 3

---
+++ ASP.NET Core 6/Modulo 3 - Creando Nuestro Primer Web API/Fin/WebAPIAutores/WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api/autores/1 
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] // api/autores/2
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ ASP.NET Core 6/Modulo 3 - Creando Nuestro Primer Web API/Fin/WebAPIAutores/WebAPIAutores/Entidades/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.Entidades
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
