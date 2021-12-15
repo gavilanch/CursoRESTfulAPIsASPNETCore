c21ae63fc406f2c643ec5619800ccab4af133831
Felipe Gavilan <elmejor_887@hotmail.com>
2021-12-15 08:22:05 -0400

    Adaptando a nueva plantilla

--- ASP.NET Core 6/Modulo 2 - Configurando el Ambiente/WebAPIAutores/WebAPIAutores/Startup.cs
+++ ASP.NET Core 6/Modulo 2 - Configurando el Ambiente/WebAPIAutores/WebAPIAutores/Startup.cs
@@ -26,8 +26,8 @@ namespace WebAPIAutores
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddEndpointsApiExplorer();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIAutores", Version = "v1" });
