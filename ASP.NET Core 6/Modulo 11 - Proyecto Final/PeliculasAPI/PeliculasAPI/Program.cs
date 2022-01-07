4f4ece65af0942c069612968a1db8550f74c47a2
Felipe Gavilan <elmejor_887@hotmail.com>
2022-01-07 09:42:09 -0400

    Mantenimiento

--- ASP.NET Core 6/Modulo 11 - Proyecto Final/PeliculasAPI/PeliculasAPI/Program.cs
+++ ASP.NET Core 6/Modulo 11 - Proyecto Final/PeliculasAPI/PeliculasAPI/Program.cs
@@ -1,26 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using PeliculasAPI;
 
-namespace PeliculasAPI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+app.Run();
--- ASP.NET Core 6/Modulo 12 - API de Suscripciones/Inicio/WebAPIAutores/WebAPIAutores/Program.cs
+++ ASP.NET Core 6/Modulo 12 - API de Suscripciones/Inicio/WebAPIAutores/WebAPIAutores/Program.cs
@@ -1,27 +1,16 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebAPIAutores.Controllers;
+using WebAPIAutores;
 
-namespace WebAPIAutores
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+var logger = app.Services.GetService(typeof(ILogger<Startup>)) as ILogger<Startup>;
+startup.Configure(app, app.Environment, logger);
+
+app.Run();
