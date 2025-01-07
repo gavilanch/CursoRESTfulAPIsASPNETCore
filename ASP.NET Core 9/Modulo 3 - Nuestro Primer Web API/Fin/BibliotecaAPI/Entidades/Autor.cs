8494103ed219aad7f6cfc607900703b71a8b2291
Felipe Gavilan <gavilanch@hotmail.com>
2025-01-07 13:59:44 -0400

    Actualizacion

---
+++ ASP.NET Core 9/Modulo 3 - Nuestro Primer Web API/Fin/BibliotecaAPI/Entidades/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPI.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Nombre { get; set; }
+        public List<Libro> Libros { get; set; } = new List<Libro>();
+    }
+}
